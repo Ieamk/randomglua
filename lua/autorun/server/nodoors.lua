abb2a597d168f91450d995a14637aafe5e5f1516
Maks <26678512+Maks-s@users.noreply.github.com>
2018-02-28 15:25:59 +0100

    Update nodoors.lua

--- lua/autorun/server/nodoors.lua
+++ lua/autorun/server/nodoors.lua
@@ -1,4 +1,4 @@
-﻿hook.Add("Initialize","noDoors", function()
+﻿hook.Add("InitPostEntity","noDoors", function()
 	timer.Simple( 1,function()
 	for _, ent in pairs(ents.GetAll()) do
 		if string.find(ent:GetName(),"door") then
